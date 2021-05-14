79f6b86a5220a1be058b12f1e5fdfbd1918de328
subrat.thakur <subrat.thakur@salesforce.com>
2021-05-14 13:28:17 +0530

    added package build

---
+++ client/build/precache-manifest.4254c094661fff23453371bedb328c54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54957af5ac703e366902a572e09cea4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ec6535536852383eec5",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "80044653704aa6c711a4",
+    "url": "/static/css/main.075d6688.chunk.css"
+  },
+  {
+    "revision": "0ec6535536852383eec5",
+    "url": "/static/js/2.dc1d96ab.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.dc1d96ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80044653704aa6c711a4",
+    "url": "/static/js/main.6bd074b1.chunk.js"
+  },
+  {
+    "revision": "cac99c31d628c94e7f7a",
+    "url": "/static/js/runtime-main.67f3c8fd.js"
+  }
+]);
