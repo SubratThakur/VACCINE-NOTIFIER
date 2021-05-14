ff4cff85d7c6e30accf29798e523a5ec1cb7d31e
subrat.thakur <subrat.thakur@salesforce.com>
2021-05-15 00:24:39 +0530

    update console.log

--- api/server.js
+++ api/server.js
@@ -105,7 +105,7 @@ const dataFetch = async (districtId,age) => {
             method: 'GET',
             headers: {'Content-Type': 'application/json','User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
         })
-      
+    console.log(response);
     const resp = await response.json();
     console.log('Center Responce: '+resp);  
     return resp;
