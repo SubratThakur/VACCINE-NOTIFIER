81f32722a143b98dad406638da24e5042114bd33
subrat.thakur <subrat.thakur@salesforce.com>
2021-05-16 08:23:16 +0530

    updated header for api

--- api/server.js
+++ api/server.js
@@ -103,6 +103,7 @@ const dataFetch = async (districtId,age) => {
   try{
     const response = await fetch(url, {
             method: 'GET',
+            // might need to add in heder 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
             headers: {'Content-Type': 'application/json'}
         })
     console.log(response);
